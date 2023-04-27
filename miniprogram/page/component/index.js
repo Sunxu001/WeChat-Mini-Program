31ec6b7ff7a232313c9190df0b4fafd80e362259
孙旭 <sunxu@sunxudeMacBook-Pro.local>
2023-04-28 00:31:13 +0500

    页面文件

---
+++ miniprogram/page/component/index.js
@@ -0,0 +1,10 @@
+Page({
+  data: {
+    //轮播图
+    movies: [
+      { url: '../../image/Carousel-blue.png' },
+      { url: '../../image/Carousel-green.png' },
+      { url: '../../image/Carousel-pink.png' },
+    ],
+}
+})
